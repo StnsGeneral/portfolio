dc703a00251106fdc691e1495b54a432ca9fc286
Tristan Miller <tristanmiller89@gmail.com>
2022-01-19 20:18:57 -0600

    Adjusted information to me

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -8,10 +8,10 @@ import {
 import { Box, Boxes, BoxNum, BoxText } from './AccomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects' },
-  { number: 1000, text: 'Students' },
-  { number: 1900, text: 'Github Followers' },
-  { number: 5000, text: 'Github Stars' },
+  { number: 16, text: 'Open Source Projects' },
+  { number: 103, text: 'Commits this year' },
+  { number: 10, text: 'Github Followers' },
+  { number: 0, text: 'Github Stars' },
 ];
 
 const Accomplishments = () => (
