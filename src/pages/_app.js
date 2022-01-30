a2c379aaa51964730606fc45dd46437c5e4e2da4
Tristan Miller <tristanmiller89@gmail.com>
2022-01-29 19:31:27 -0500

    Added title to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,12 @@ import Theme from '../styles/theme';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <head>
+        <title>Tristan Miller's Portfolio</title>
+      </head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
     </>
   );
 }
- 
